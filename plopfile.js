bb898918b802f71e703b6b219ab38b391dc5b120
初志鑫 <1204505056@qq.com>
2020-04-30 19:12:09 +0800

    更新代码生成机

--- plopfile.js
+++ plopfile.js
@@ -1,9 +1,12 @@
 const viewGenerator = require("./templates/view/prompt");
+const curdGenerator = require("./templates/curd/prompt");
 const componentGenerator = require("./templates/component/prompt");
 const mockGenerator = require("./templates/mock/prompt");
 const vuexGenerator = require("./templates/vuex/prompt");
+
 module.exports = function (plop) {
   plop.setGenerator("view", viewGenerator);
+  plop.setGenerator("curd", curdGenerator);
   plop.setGenerator("component", componentGenerator);
   plop.setGenerator("mock&api", mockGenerator);
   plop.setGenerator("vuex", vuexGenerator);
