b778357aad79def033dcf0ebdf6cc31607e17ec1
初志鑫 <1204505056@qq.com>
2020-04-30 19:10:38 +0800

    更新代码生成机

--- templates/component/prompt.js
+++ templates/component/prompt.js
@@ -11,33 +11,6 @@ module.exports = {
         "\u8bf7\u8f93\u5165\u0063\u006f\u006d\u0070\u006f\u006e\u0065\u006e\u0074\u540d\u79f0\u002c\u52ff\u4e0e\u4e4b\u524d\u91cd\u590d\u002c\u7136\u540e\u70b9\u51fb\u56de\u8f66",
       validate: notEmpty("name"),
     },
-    /* {
-          type: 'checkbox',
-          name: 'blocks',
-          message: 'Blocks:',
-          choices: [{
-              name: '<template>',
-              value: 'template',
-              checked: true
-            },
-            {
-              name: '<script>',
-              value: 'script',
-              checked: true
-            },
-            {
-              name: 'style',
-              value: 'style',
-              checked: true
-            }
-          ],
-          validate(value) {
-            if (value.indexOf('script') === -1 && value.indexOf('template') === -1) {
-              return 'Components require at least a <script> or <template> tag.';
-            }
-            return true;
-          }
-        } */
   ],
   actions: (data) => {
     const name = "{{properCase name}}";
@@ -48,9 +21,6 @@ module.exports = {
         templateFile: "templates/component/index.hbs",
         data: {
           name: name,
-          /*  template: data.blocks.includes('template'),
-                  script: data.blocks.includes('script'),
-                  style: data.blocks.includes('style') */
         },
       },
     ];
