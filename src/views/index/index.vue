5d175ba07e4420b5a175d1070c39089dc393b86e
初志鑫 <1204505056@qq.com>
2020-05-03 12:13:27 +0800

    更新首页

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="15">
       <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
         <el-alert
-          title="鸣谢：花裤衩、唐金州、贤心的开源项目让我学到了很多知识也迸发了很多的灵感"
+          title="作者初衷：iView Pro要卖￥9999，layui Admin要卖￥2499，作者买不起只好自己动手写一个咯，免费的东西一定就差吗，一个人就一定比不过一个团队吗？我还那么年轻，我不会认输，在屏幕前驻足的你也不要轻易认输，好吗？"
           :closable="false"
         >
         </el-alert>
@@ -210,6 +210,13 @@
           <el-alert
             :closable="false"
             title="框架优势：mock数据自动导出无需配置；views，vuex，api支持自动生成；自动fixed问题代码，可以愉快的拥抱eslint"
+            type="success"
+          >
+          </el-alert>
+          <br />
+          <el-alert
+            :closable="false"
+            title="鸣谢花裤衩、唐金州、贤心、element、ivew的开源给我带来的很多的灵感"
             type="success"
           >
           </el-alert>
@@ -572,17 +579,17 @@ export default {
         this.activities = res.data;
       });
       /*getRepos({
-            access_token: "1061286824f978ea3cf98b7b8ea26fe27ba7cea1",
-          }).then((res) => {
-            const per_page = res.stargazers_count;
-            getStargazers({
-              access_token: "1061286824f978ea3cf98b7b8ea26fe27ba7cea1",
-              page: 1,
-              per_page: res.per_page,
-            }).then((res) => {
-              alert(JSON.stringify(res));
-            });
-          });*/
+                              access_token: "1061286824f978ea3cf98b7b8ea26fe27ba7cea1",
+                            }).then((res) => {
+                              const per_page = res.stargazers_count;
+                              getStargazers({
+                                access_token: "1061286824f978ea3cf98b7b8ea26fe27ba7cea1",
+                                page: 1,
+                                per_page: res.per_page,
+                              }).then((res) => {
+                                alert(JSON.stringify(res));
+                              });
+                            });*/
     },
   },
 };
