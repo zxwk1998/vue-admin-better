aa55d876fa94cafc8914ae443f37181da41e4792
初志鑫 <1204505056@qq.com>
2020-05-26 20:16:18 +0800

    添加雪花屏

--- mock/controller/changeLog.js
+++ mock/controller/changeLog.js
@@ -101,6 +101,10 @@ const data = [
     content: "又被人骂了 挺好的 让我下定决心重写了tagsBar",
     timestamp: "2020-05-20",
   },
+  {
+    content: "仿ant-design 添加雪花屏",
+    timestamp: "2020-05-26",
+  },
 ];
 
 export default [
