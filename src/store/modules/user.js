66534241cac088c6581222904acc5a5aa899cae6
初志鑫 <1204505056@qq.com>
2020-05-28 21:15:32 +0800

    deploy

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -38,20 +38,27 @@ const actions = {
   async login({ commit }, userInfo) {
     const { data } = await login(userInfo);
     const accessToken = data[tokenName];
-    commit("setAccessToken", accessToken);
-    setAccessToken(accessToken);
-    const hour = new Date().getHours();
-    const thisTime =
-      hour < 8
-        ? "早上好"
-        : hour <= 11
-        ? "上午好"
-        : hour <= 13
-        ? "中午好"
-        : hour < 18
-        ? "下午好"
-        : "晚上好";
-    Vue.prototype.$baseNotify(`欢迎登录${title}`, `${thisTime}！`);
+    if (accessToken) {
+      commit("setAccessToken", accessToken);
+      setAccessToken(accessToken);
+      const hour = new Date().getHours();
+      const thisTime =
+        hour < 8
+          ? "早上好"
+          : hour <= 11
+          ? "上午好"
+          : hour <= 13
+          ? "中午好"
+          : hour < 18
+          ? "下午好"
+          : "晚上好";
+      Vue.prototype.$baseNotify(`欢迎登录${title}`, `${thisTime}！`);
+    } else {
+      Vue.prototype.$baseMessage(
+        `登录接口异常，未正确返回${tokenName}...`,
+        "error"
+      );
+    }
   },
   async getInfo({ commit, state }) {
     const { data } = await getInfo(state.accessToken);
