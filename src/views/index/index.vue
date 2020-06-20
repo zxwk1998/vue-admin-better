093e73a58636ad7777dd2ea6ed427732a9c699c5
初志鑫 <1204505056@qq.com>
2020-06-20 21:58:04 +0800

    将首页地图移动到echarts组件

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -187,30 +187,7 @@
           </div>
         </el-card>
       </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
-        <el-card class="card" shadow="never">
-          <div slot="header">
-            <span>中国地图</span>
-          </div>
-          <vab-chart
-            :autoresize="true"
-            theme="vab-echarts-theme"
-            :options="zgdt"
-          />
-        </el-card>
-      </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
-        <el-card class="card" shadow="never">
-          <div slot="header">
-            <span>世界地图</span>
-          </div>
-          <vab-chart
-            :autoresize="true"
-            theme="vab-echarts-theme"
-            :options="sjdt"
-          />
-        </el-card>
-      </el-col>
+
       <el-col
         v-for="(item, index) in iconList"
         :key="index"
@@ -266,13 +243,6 @@
           <div slot="header">
             <span>其他信息</span>
           </div>
-          <!--  <el-alert
-            :closable="false"
-            title="2020年5月12日，不同寻常的一天，汶川大地震的12周年，被社区一名叫做812770127@qq.com的网友骂的狗血淋头，说这是一个抄袭的项目，这是一个一点技术含量都没有的项目，没关系，我把这段话一直留在这里，总有一天你会看到这个你曾经不屑一顾的框架发展壮大，谢谢你的嘲讽让我变得强大"
-            type="error"
-          >
-          </el-alert>
-          <br /> -->
           <div v-for="(item, index) in noticeList" :key="index">
             <el-alert
               v-if="index !== 0"
@@ -596,121 +566,7 @@ export default {
           },
         ],
       },
-      //中国地图
-      zgdt: {
-        title: {
-          text: "2099年全国GDP分布",
-          subtext: "数据来自vue-admin-beautiful杜撰",
-        },
-        tooltip: {
-          trigger: "item",
-        },
-        dataRange: {
-          orient: "horizontal",
-          min: 0,
-          max: 55000,
-          text: ["高", "低"],
-          splitNumber: 0,
-        },
-        series: [
-          {
-            name: "2099年全国GDP分布",
-            type: "map",
-            roam: false,
-            zoom: 1.25,
-            mapType: "china",
-            mapLocation: {
-              x: "center",
-            },
-            selectedMode: "multiple",
-            itemStyle: {
-              normal: {
-                label: {
-                  show: false,
-                },
-              },
-              emphasis: {
-                label: {
-                  show: true,
-                },
-              },
-            },
-            data: [
-              { name: "西藏", value: 605.83 },
-              { name: "青海", value: 1670.44 },
-              { name: "宁夏", value: 2102.21 },
-              { name: "海南", value: 2522.66 },
-              { name: "甘肃", value: 5020.37 },
-              { name: "贵州", value: 5701.84 },
-              { name: "新疆", value: 6610.05 },
-              { name: "云南", value: 8893.12 },
-              { name: "重庆", value: 10011.37 },
-              { name: "吉林", value: 10568.83 },
-              { name: "山西", value: 11237.55 },
-              { name: "天津", value: 11307.28 },
-              { name: "江西", value: 11702.82 },
-              { name: "广西", value: 11720.87 },
-              { name: "陕西", value: 12512.3 },
-              { name: "黑龙江", value: 12582 },
-              { name: "内蒙古", value: 14359.88 },
-              { name: "安徽", value: 15300.65 },
-              { name: "北京", value: 16251.93 },
-              { name: "福建", value: 17560.18 },
-              { name: "上海", value: 19195.69 },
-              { name: "湖北", value: 19632.26 },
-              { name: "湖南", value: 19669.56 },
-              { name: "四川", value: 21026.68 },
-              { name: "辽宁", value: 22226.7 },
-              { name: "河北", value: 24515.76 },
-              { name: "河南", value: 26931.03 },
-              { name: "浙江", value: 32318.85 },
-              { name: "山东", value: 45361.85, selected: true },
-              { name: "江苏", value: 49110.27 },
-              { name: "广东", value: 53210.28 },
-            ],
-          },
-        ],
-      },
-      //世界地图
-      sjdt: {
-        title: {
-          text: "2099年世界GDP（亿元）",
-          subtext: "数据来自vue-admin-beautiful杜撰",
-        },
-        tooltip: {
-          trigger: "item",
-        },
-        dataRange: {
-          orient: "horizontal",
-          min: 0,
-          max: 55000,
-          text: ["高", "低"],
-          splitNumber: 0,
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{b}",
-        },
-        series: [
-          {
-            name: "2099年世界GDP（亿元）",
-            type: "map",
-            mapType: "world",
-            selectedMode: "multiple",
-            roam: false,
-            zoom: 1.15,
-            label: {
-              normal: {
-                show: false,
-              },
-              emphasis: {
-                show: true,
-              },
-            },
-            data: [{ name: "China", value: 99999, selected: true }],
-          },
-        ],
-      },
+
       //更新日志
       reverse: true,
       activities: [],
--- src/views/vab/echarts/index.vue
+++ src/views/vab/echarts/index.vue
@@ -9,7 +9,6 @@
           </div>
         </el-card>
       </el-col>
-
       <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8">
         <el-card shadow="hover">
           <div slot="header">环形图</div>
@@ -35,6 +34,30 @@
               class="my-line1"
             />
           </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+        <el-card class="card" shadow="never">
+          <div slot="header">
+            <span>中国地图</span>
+          </div>
+          <vab-chart
+            :autoresize="true"
+            theme="vab-echarts-theme"
+            :options="zgdt"
+          />
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+        <el-card class="card" shadow="never">
+          <div slot="header">
+            <span>世界地图</span>
+          </div>
+          <vab-chart
+            :autoresize="true"
+            theme="vab-echarts-theme"
+            :options="sjdt"
+          />
         </el-card>
       </el-col>
     </el-row>
@@ -501,6 +524,119 @@ export default {
                 name: "法人",
               },
             ],
+          },
+        ],
+      },
+      //中国地图
+      zgdt: {
+        title: {
+          text: "2099年全国GDP分布",
+          subtext: "数据来自vue-admin-beautiful杜撰",
+        },
+        tooltip: {
+          trigger: "item",
+        },
+        dataRange: {
+          orient: "horizontal",
+          min: 0,
+          max: 55000,
+          text: ["高", "低"],
+          splitNumber: 0,
+        },
+        series: [
+          {
+            name: "2099年全国GDP分布",
+            type: "map",
+            roam: false,
+            mapType: "china",
+            mapLocation: {
+              x: "center",
+            },
+            selectedMode: "multiple",
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false,
+                },
+              },
+              emphasis: {
+                label: {
+                  show: true,
+                },
+              },
+            },
+            data: [
+              { name: "西藏", value: 605.83 },
+              { name: "青海", value: 1670.44 },
+              { name: "宁夏", value: 2102.21 },
+              { name: "海南", value: 2522.66 },
+              { name: "甘肃", value: 5020.37 },
+              { name: "贵州", value: 5701.84 },
+              { name: "新疆", value: 6610.05 },
+              { name: "云南", value: 8893.12 },
+              { name: "重庆", value: 10011.37 },
+              { name: "吉林", value: 10568.83 },
+              { name: "山西", value: 11237.55 },
+              { name: "天津", value: 11307.28 },
+              { name: "江西", value: 11702.82 },
+              { name: "广西", value: 11720.87 },
+              { name: "陕西", value: 12512.3 },
+              { name: "黑龙江", value: 12582 },
+              { name: "内蒙古", value: 14359.88 },
+              { name: "安徽", value: 15300.65 },
+              { name: "北京", value: 16251.93 },
+              { name: "福建", value: 17560.18 },
+              { name: "上海", value: 19195.69 },
+              { name: "湖北", value: 19632.26 },
+              { name: "湖南", value: 19669.56 },
+              { name: "四川", value: 21026.68 },
+              { name: "辽宁", value: 22226.7 },
+              { name: "河北", value: 24515.76 },
+              { name: "河南", value: 26931.03 },
+              { name: "浙江", value: 32318.85 },
+              { name: "山东", value: 45361.85, selected: true },
+              { name: "江苏", value: 49110.27 },
+              { name: "广东", value: 53210.28 },
+            ],
+          },
+        ],
+      },
+      //世界地图
+      sjdt: {
+        title: {
+          text: "2099年世界GDP（亿元）",
+          subtext: "数据来自vue-admin-beautiful杜撰",
+        },
+        tooltip: {
+          trigger: "item",
+        },
+        dataRange: {
+          orient: "horizontal",
+          min: 0,
+          max: 55000,
+          text: ["高", "低"],
+          splitNumber: 0,
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{b}",
+        },
+        series: [
+          {
+            name: "2099年世界GDP（亿元）",
+            type: "map",
+            mapType: "world",
+            selectedMode: "multiple",
+            roam: false,
+            label: {
+              normal: {
+                show: false,
+              },
+              emphasis: {
+                show: true,
+              },
+            },
+            data: [{ name: "China", value: 99999, selected: true }],
           },
         ],
       },
