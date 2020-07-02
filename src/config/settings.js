222c084a2591a98ba23148f7d55c9e32ef66b366
初志鑫 <1204505056@qq.com>
2020-07-02 16:20:35 +0800

    默认移除骨架屏

--- src/config/settings.js
+++ src/config/settings.js
@@ -61,7 +61,7 @@ module.exports = {
   //是否显示多标签页
   tagsBar: true,
   //是否显示骨架屏
-  skeleton: true,
+  skeleton: false,
   //配后端数据的接收方式application/json;charset=UTF-8或者application/x-www-form-urlencoded;charset=UTF-8
   contentType: "application/json;charset=UTF-8",
   //消息框消失时间
