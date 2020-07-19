29a9fba4cd9f6881c9096c13c6ef24df36c183fb
初志鑫 <1204505056@qq.com>
2020-07-19 20:04:53 +0800

    主题配置大版本迭代授权版本增加海洋之心、绿荫操场、荣耀典藏主题

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -6,7 +6,8 @@
 import defaultSettings from "@/config/settings";
 
 const { tagsBar, logo, layout, header, themeBar, skeleton } = defaultSettings;
-const theme = JSON.parse(localStorage.getItem("BYUI-VUE-THEME")) || "";
+const theme =
+  JSON.parse(localStorage.getItem("vue-admin-beautiful-theme")) || "";
 const state = {
   tagsBar: theme.tagsBar || tagsBar,
   logo,
