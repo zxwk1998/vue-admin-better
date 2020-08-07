98968689ee58b8b35f197cf82cdef4cc9f3d9dd3
chuzhixin <1204505056@qq.com>
2020-08-07 17:08:55 +0800

    deploy

--- mock/controller/ad.js
+++ mock/controller/ad.js
@@ -1,20 +1,12 @@
 const data = [
   {
+    title: "vue-admin-beautiful-pro beta版本已发布，点我提前体验",
+    url: "http://beautiful.panm.cn/vue-admin-beautiful-pro",
+  },
+  {
     title:
       "作为一个程序员，我迄今为止最骄傲的事情：2020年7月10日，vue/cli4作者蒋豪群采纳了我的对sass-loader 9.0全局注入变量的文档修改建议，以后全世界都能看到我的名字了，这远比vue-admin-beautiful更让我有成就感，感谢shaonialife的帮助。",
     url: "https://github.com/vuejs/vue-cli/blob/master/docs/zh/guide/css.md",
-  },
-  {
-    title:
-      "近日发现有人打着福州客家网络科技的名义发布了一款名为芊雅admin的项目，不仅完全照搬了vab基础版并对授权文件进行破解并进行二次改造二次收费，将vab的作者信息完全替换，同时也将框架原来存在的bug完整的拷贝了过去",
-    url:
-      "//shang.qq.com/wpa/qunwpa?idkey=14f123ac6d4ef3122bbb60d4693f1d8c951a50be2296951efb12d5ab1642f36b",
-  },
-  {
-    title:
-      "近日发现一个名为OKMG芒果源码的网站公然出售vue-admin-beautiful的开源代码，在此向大家说明，框架开源版本永久免费，请勿上当受骗。",
-    url:
-      "//shang.qq.com/wpa/qunwpa?idkey=14f123ac6d4ef3122bbb60d4693f1d8c951a50be2296951efb12d5ab1642f36b",
   },
   {
     title: "uView UI：全面的组件和便捷的工具会让您信手拈来，如鱼得水。",
