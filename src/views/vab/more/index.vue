6842c4f4afd1a588f6099335e5caeb625fb27e6f
chuzhixin <1204505056@qq.com>
2020-08-09 10:58:12 +0800

    deploy

--- src/views/vab/more/index.vue
+++ src/views/vab/more/index.vue
@@ -14,7 +14,7 @@
           </div>
           <div>
             <ul>
-              <li>永久开源免费</li>
+              <li>永久开源免费，支持横纵布局切换</li>
               <li>保留控制台以及代码中的作者信息可免费商用</li>
               <li>
                 开源地址
@@ -29,7 +29,6 @@
                 提供讨论群专属文档，QQ群 972435319
               </li>
             </ul>
-            <img src="@/assets/ewm.png" alt="" />
           </div>
         </el-card>
       </el-col>
@@ -44,6 +43,7 @@
           <div>
             <ul>
               <li>支持以上所有特权</li>
+              <li>支持四种主题</li>
               <li>
                 提供vip群专属文档及视频教程，可快速入手框架，包含打包优化，公共布局zx-layouts
                 npm包本地化教程等
@@ -56,20 +56,51 @@
       <el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
         <el-card>
           <div slot="header">
-            商业用途 完全自定义版权
+            开源版授权 商业用途 完全自定义版权
             <el-button style="float: right; padding: 3px 0;" type="text">
               ￥299
             </el-button>
           </div>
           <div>
             <ul>
-              <li>支持以上所有特权</li>
+              <li>支持以上所有特权，不包含VIP群</li>
               <li>
-                可随意变更版权，但仅限自己团队使用，一旦发现恶意传播，二次售卖，不再提供更新支持
+                可随意变更版权，但仅限自己团队使用，禁止恶意传播，禁止二次售卖
               </li>
+              <li>包含开源基础版授权与开源集成版授权</li>
               <li>永久更新</li>
-              <li>优先问题解答</li>
               <li>提供低价外包合作机会</li>
+            </ul>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
+        <el-card>
+          <div slot="header">
+            PRO版
+            <el-button style="float: right; padding: 3px 0;" type="text">
+              ￥499
+            </el-button>
+          </div>
+          <div>
+            <ul>
+              <li>
+                演示地址：
+                <a
+                  target="_blank"
+                  href="https://chu1204505056.gitee.io/vue-admin-beautiful-pro"
+                >
+                  点我
+                </a>
+              </li>
+              <li>PRO独立版本与开源版本不同</li>
+              <li>
+                无开源版限制，无版权限制，无需额外配置，拿来即用
+              </li>
+              <li>免费加入vue-admin-beautifl github团队</li>
+              <li>提供三种布局方式、四种主题搭配</li>
+              <li>图标使用方式大为简化</li>
+              <li>支持国际化语言包、中英文无缝切换（此后将支持13国语言）</li>
             </ul>
           </div>
         </el-card>
@@ -95,7 +126,7 @@
     ::v-deep {
       .el-card__body {
         > div {
-          min-height: 70vh;
+          min-height: 220px;
           padding: 20px;
 
           > ul {
