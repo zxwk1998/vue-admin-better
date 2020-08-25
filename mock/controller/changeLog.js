942b673ad6a69561db93115dad284bdb40175407
chuzhixin <1204505056@qq.com>
2020-08-25 18:17:43 +0800

    添加更新日志

--- mock/controller/changeLog.js
+++ mock/controller/changeLog.js
@@ -166,6 +166,10 @@ const data = [
     content: "重构全局loadding加载代码",
     timestamp: "2020-07-31",
   },
+  {
+    content: "升级stylelint自动排序功能",
+    timestamp: "2020-08-25",
+  },
 ];
 
 module.exports = [
