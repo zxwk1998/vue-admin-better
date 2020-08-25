f40d15a770bb25ed3b77ef7ff1c7452b231b8578
chuzhixin <1204505056@qq.com>
2020-08-25 17:34:03 +0800

    添加stylint自动排序

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,35 +1,3 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-  rules: {
-    "at-rule-no-unknown": [
-      true,
-      {
-        ignoreAtRules: [
-          "mixin",
-          "extend",
-          "content",
-          "include",
-          "for",
-          "function",
-          "return",
-        ],
-      },
-    ],
-    "selector-pseudo-element-no-unknown": [
-      true,
-      {
-        ignorePseudoElements: ["v-deep"],
-      },
-    ],
-    "selector-pseudo-class-no-unknown": [
-      true,
-      {
-        ignorePseudoClasses: ["export"],
-      },
-    ],
-    indentation: 2,
-    "no-descending-specificity": null,
-    "declaration-colon-newline-after": null,
-  },
-  ignoreFiles: ["**/*.js", "dist/*.*", "node_modules", "**/*.ts", "**/*.md"],
+  extends: ["stylelint-config-recess-order", "stylelint-config-prettier"],
 };
