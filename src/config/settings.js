d8e3cba8b0b448f65cc58dc5055f1cca0b1ddd47
fwfmiao <liu-fengyu@outlook.com>
2020-08-29 18:36:53 +0800

    fix: router history mode 404 error

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,8 +3,8 @@
  * @description 全局变量配置
  */
 module.exports = {
-  // 开发以及部署时的URL 如"/test/"
-  publicPath: "",
+  // 开发以及部署时的URL
+  publicPath: "/",
   // 生产环境构建文件的目录名
   outputDir: "dist",
   // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
