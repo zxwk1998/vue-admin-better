86880668272ff9d2169bde4b828c575e0ffa5c66
chuzhixin <1204505056@qq.com>
2020-10-01 10:44:47 +0800

    🔥 首页添加鸣谢信息

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,7 +3,20 @@
     <el-row :gutter="20">
       <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
         <el-alert v-if="noticeList[0]" :closable="noticeList[0].closable">
-          {{ noticeList[0].title }}
+          <div
+            style="display: flex; align-items: center; justify-content: center"
+          >
+            <a
+              target="_blank"
+              href="https://github.com/chuzhixin/vue-admin-beautiful"
+            >
+              <img
+                style="margin-right: 10px"
+                src="https://img.shields.io/github/stars/chuzhixin/vue-admin-beautiful?style=flat-square&label=Stars&logo=github"
+              />
+            </a>
+            {{ noticeList[0].title }}
+          </div>
         </el-alert>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="6" :xl="6">
