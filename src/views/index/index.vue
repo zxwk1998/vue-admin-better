f1d88728f6b22db599893aed4751021603670f37
chuzhixin <1204505056@qq.com>
2020-10-04 16:12:43 +0800

    ✨更新首页

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="index-container">
+    <a
+      target="_blank"
+      href="https://github.com/chuzhixin/vue-admin-beautiful/tree/vue3.0-antdv"
+    >
+      <img
+        style="margin-right: 10px"
+        src="https://img.shields.io/github/stars/chuzhixin/vue-admin-beautiful?style=flat-square&label=Stars&logo=github"
+      />
+    </a>
     <version-information />
   </div>
 </template>
