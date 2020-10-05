9d44b56b22d89859167a1d353ab71f6df9026954
chuzhixin <1204505056@qq.com>
2020-10-05 18:01:01 +0800

    ✨ 添加gh-pages部署

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f "https://${access_token}@gitee.com/chu1204505056/vue-admin-beautiful-mini.git" master:gh-pages
+git push -f "https://${access_token}@gitee.com/chu1204505056/vue-admin-beautiful-antdv.git" master:gh-pages
 start "https://gitee.com/chu1204505056/vue-admin-beautiful-mini/pages"
+start "https://gitee.com/chu1204505056/vue-admin-beautiful-antdv/pages"
 cd -
 exec /bin/bash
 
