6ee1852121f16aa5703912469cdff52fa188899e
chuzhixin <1204505056@qq.com>
2020-10-15 23:05:56 +0800

    deploy

--- mock/controller/ad.js
+++ mock/controller/ad.js
@@ -1,11 +1,11 @@
 const data = [
   {
-    title: 'vue-admin-beautiful-pro 1.7版本已发布，点我提前体验',
+    title: 'vue-admin-beautiful-pro 1.9版本已发布，点我提前体验',
     url: 'https://chu1204505056.gitee.io/vue-admin-beautiful-pro/#/index',
   },
   {
     title: 'vue-admin-beautiful（antdv） vue3.0版本已发布，点我提前体验',
-    url: 'https://chu1204505056.gitee.io/vue-admin-beautiful-mini/#/index',
+    url: 'https://chu1204505056.gitee.io/vue-admin-beautiful-antdv/#/index',
   },
 ]
 module.exports = [
