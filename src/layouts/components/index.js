ccd32238e5dea6a7151b10648bf579fdae85dbaf
chuzhixin <1204505056@qq.com>
2020-10-23 10:16:37 +0800

    ♻ feat: Auto export public layout

--- src/layouts/components/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
- * @description 公共js导出
- */
-
-export { default as ColorfullIcon } from 'zx-layouts/ColorfullIcon'
-export { default as RemixIcon } from 'zx-layouts/RemixIcon'
