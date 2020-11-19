8b082f91e2e574853d5c49dccdecd99562279ac0
chuzhixin <1204505056@qq.com>
2020-11-19 10:09:35 +0800

    deploy

--- mock/controller/ad.js
+++ mock/controller/ad.js
@@ -1,6 +1,7 @@
 const data = [
   {
-    title: 'vue-admin-beautiful-pro 1.9版本已发布，点我提前体验',
+    title:
+      'vue-admin-beautiful-pro 2.0版本已发布，增加多项贴心功能，点我提前体验',
     url: 'https://chu1204505056.gitee.io/vue-admin-beautiful-pro/#/index',
   },
   {
