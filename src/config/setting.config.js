586506208a2e86f3081e7b2b7cb761925aaab2be
chuzhixin <1204505056@qq.com>
2021-01-11 23:18:38 +0800

    🚀 feat: Open Rsa

--- src/config/setting.config.js
+++ src/config/setting.config.js
@@ -54,7 +54,7 @@ const setting = {
   //是否开启登录拦截
   loginInterception: true,
   //是否开启登录RSA加密
-  loginRSA: false,
+  loginRSA: true,
   //intelligence和all两种方式，前者后端权限只控制permissions不控制view文件的import（前后端配合，减轻后端工作量），all方式完全交给后端前端只负责加载
   authentication: 'intelligence',
   //vertical布局时是否只保持一个子菜单的展开
