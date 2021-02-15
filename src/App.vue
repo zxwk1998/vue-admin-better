525ae624181e950551b8171cc351b30f391330b4
hdtopku <1450948930@qq.com>
2021-02-15 15:19:41 +0800

    Update App.vue
    汉化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <router-view />
+  <div id="vue-admin-beautiful">
+    <a-config-provider :locale="zh_CN">
+      <router-view />
+    </a-config-provider>
+  </div>
 </template>
+<script>
+  import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
+  import moment from 'moment'
+  import 'moment/locale/zh-cn'
+  moment.locale('zh-cn')
+  export default {
+    name: 'App',
+    data() {
+      return {
+        zh_CN,
+      }
+    },
+  }
+</script>
 <style lang="less">
   @import '~@/vab/styles/vab.less';
 </style>
