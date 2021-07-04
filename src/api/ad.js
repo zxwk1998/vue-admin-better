cbd437e21d6034f67a101415b7b78f4840177d98
vue-admin-beautiful.com <783963206@qq.com>
2021-07-04 22:46:33 +0800

    deploy

--- src/api/ad.js
+++ src/api/ad.js
@@ -2,7 +2,8 @@ import request from '@/utils/request'
 
 export function getList(data) {
   return request({
-    url: '/ad/getList',
+    //url: '/ad/getList',
+    url: 'https://851edf02-46eb-43e6-828d-64c7e483ea41.bspapp.com/http/getAd',
     method: 'get',
     data,
   })
--- src/config/setting.config.js
+++ src/config/setting.config.js
@@ -22,7 +22,7 @@ const setting = {
   //简写
   abbreviation: 'vab',
   //开发环境端口号
-  devPort: '80',
+  devPort: '81',
   //版本号
   version: process.env.VUE_APP_VERSION,
   //这一项非常重要！请务必保留MIT协议下package.json及copyright作者信息 即可免费商用，不遵守此项约定你将无法使用该框架，如需自定义版权信息请联系QQ1204505056
