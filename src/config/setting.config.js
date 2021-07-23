bd0332645d9fb4e8faa04ec94f45bc49f8fab636
zxwk <chuzhixin@foxmail.com>
2021-07-23 11:03:09 +0800

    ♻ refactor: add baseURL

--- src/config/setting.config.js
+++ src/config/setting.config.js
@@ -12,11 +12,6 @@ const setting = {
   lintOnSave: true,
   // 进行编译的依赖
   transpileDependencies: [],
-  // 默认的接口地址 如果是开发环境和生产环境走vab-mock-server，当然你也可以选择自己配置成需要的接口地址
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'vab-mock-server'
-      : 'vab-mock-server',
   //标题 （包括初次加载雪花屏的标题 页面的标题 浏览器的标题）
   title: 'vue-admin-beautiful-pro',
   //简写
