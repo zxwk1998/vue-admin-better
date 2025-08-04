e0b21cfce0f755d0f8168e41d0074b135fd37810
sundan <chu1204505056@126.com>
2025-08-04 23:19:16 +0800

    ♻️ refactor:  code

--- src/layouts/components/VabAd/index.vue
+++ src/layouts/components/VabAd/index.vue
@@ -2,7 +2,7 @@
   <div class="vab-ad">
     <el-carousel v-if="adList" :autoplay="true" :interval="3000" direction="vertical" height="30px" indicator-position="none">
       <el-carousel-item v-for="(item, index) in adList" :key="index">
-        <el-tag type="warning">Ad</el-tag>
+        <el-tag type="warning">付费版本 Ad</el-tag>
         <a :href="item.url" target="_blank">{{ item.title }}</a>
       </el-carousel-item>
     </el-carousel>
@@ -32,15 +32,25 @@
 </script>
 <style lang="scss" scoped>
   .vab-ad {
+    margin-top: 7px;
     height: 30px;
     padding-right: $base-padding;
     padding-left: $base-padding;
     margin-bottom: -20px;
-    line-height: 30px;
+    line-height: 32px;
     cursor: pointer;
 
     a {
-      color: #999;
+      margin-left: 5px;
+      color: #888;
+    }
+
+    ::v-deep {
+      .el-carousel__container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
     }
   }
 </style>
