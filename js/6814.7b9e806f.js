b5dc37beba1b057aea0daac37dba55cc238bf478
zxwk1998 <zxwk1998@users.noreply.github.com>
2025-08-26 04:02:56 +0000

    deploy: ed772353970cb0b5cd36d04584cc202aa00558fe

---
+++ js/6814.7b9e806f.js
@@ -0,0 +1,25 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-8-26 04:02:49
+ */
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["6814"], {
+61698: (function (module, __unused_webpack_exports, __webpack_require__) {
+/**
+ * @description 3个子配置，通用配置|主题配置|网络配置
+ */
+//默认配置
+const {
+  setting,
+  theme,
+  network
+} = __webpack_require__(84465);
+module.exports = Object.assign({}, setting, theme, network);
+
+}),
+
+}]);
