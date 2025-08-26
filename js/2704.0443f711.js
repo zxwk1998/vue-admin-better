5b8abe3f14a71efc377ac9b636340325da8a5960
zxwk1998 <zxwk1998@users.noreply.github.com>
2025-08-26 04:04:44 +0000

    deploy: a1df9c19ac5a9696300edc6c8c9a867513e82697

---
+++ js/2704.0443f711.js
@@ -0,0 +1,17 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-8-26 04:04:38
+ */
+"use strict";
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["2704"], {
+32467: (function (module) {
+module.exports = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+CiAgPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgyNHYyNEgweiIvPgogIDxwYXRoIGQ9Ik0xIDNoNGw3IDEyIDctMTJoNEwxMiAyMiAxIDN6bTguNjY3IDBMMTIgN2wyLjMzMy00aDQuMDM1TDEyIDE0IDUuNjMyIDNoNC4wMzV6Ii8+Cjwvc3ZnPgo=";
+
+}),
+
+}]);
