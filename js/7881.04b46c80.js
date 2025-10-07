e3866a4c4914ca0fb54d4dad3ca571451b8f212e
zxwk1998 <zxwk1998@users.noreply.github.com>
2025-10-07 10:14:23 +0000

    deploy: 5cddadfd68349e8bfb3565bfb09095d223cf7ab9

---
+++ js/7881.04b46c80.js
@@ -0,0 +1,28 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-10-7 10:14:16
+ */
+"use strict";
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["7881"], {
+10304: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  getNoticeList: function() { return getNoticeList; }
+});
+/* ESM import */var _utils_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(29842);
+
+function getNoticeList() {
+  return (0,_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    url: 'https://api.vuejs-core.cn/getNotice',
+    method: 'get'
+  });
+}
+
+}),
+
+}]);
---
+++ js/9339.8a26319a.js
@@ -0,0 +1,29 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-10-7 10:14:16
+ */
+"use strict";
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["9339"], {
+75430: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  getIconList: function() { return getIconList; }
+});
+/* ESM import */var _utils_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(29842);
+
+function getIconList(data) {
+  return (0,_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    url: '/remixIcon/getList',
+    method: 'post',
+    data
+  });
+}
+
+}),
+
+}]);
