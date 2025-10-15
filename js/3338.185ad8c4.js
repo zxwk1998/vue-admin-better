477b30265caa18c03f154150bc38d8b1c53cd7d3
zxwk1998 <zxwk1998@users.noreply.github.com>
2025-10-15 16:31:32 +0000

    deploy: 5f1ef7113bfd5ea359ec269bd714f23edb11aa90

---
+++ js/3338.185ad8c4.js
@@ -0,0 +1,45 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-10-15 16:31:26
+ */
+"use strict";
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["3338"], {
+85649: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  doDelete: function() { return doDelete; },
+  doEdit: function() { return doEdit; },
+  getList: function() { return getList; }
+});
+/* ESM import */var _utils_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(29842);
+
+function getList(data) {
+  return (0,_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    url: '/personalCenter/getList',
+    method: 'post',
+    data
+  });
+}
+function doEdit(data) {
+  return (0,_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    url: '/personalCenter/doEdit',
+    method: 'post',
+    data
+  });
+}
+function doDelete(data) {
+  return (0,_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    url: '/personalCenter/doDelete',
+    method: 'post',
+    data
+  });
+}
+
+}),
+
+}]);
