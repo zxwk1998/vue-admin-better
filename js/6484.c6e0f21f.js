d937d56e251da187b257c52df0daab530cb27933
zxwk1998 <zxwk1998@users.noreply.github.com>
2025-12-22 10:58:10 +0000

    deploy: bc8bb6dce9aa5082043b88cad6d4ac962154ee60

---
+++ js/6484.c6e0f21f.js
@@ -0,0 +1,71 @@
+/*!
+ * vue-admin-better
+ * GitHub: https://github.com/zxwk1998/vue-admin-better
+ * Gitee: https://gitee.com/chu1204505056/vue-admin-better
+ *
+ * 版权所有 (c) 2025 vue-admin-better
+ * 本项目使用 MIT 许可证
+ * 构建时间: 2025-12-22 10:58:04
+ */
+"use strict";
+(self["webpackChunkvue_admin_better"] = self["webpackChunkvue_admin_better"] || []).push([["6484"], {
+74671: (function (__unused_rspack_module, __webpack_exports__, __webpack_require__) {
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ test; }
+});
+
+;// CONCATENATED MODULE: ./node_modules/.pnpm/vue-loader@15.9.8_@vue+compiler-sfc@3.5.26_css-loader@7.1.2_@rspack+core@1.6.8_webpack@_627e006248d04719becae5a8a0ffece3/node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/.pnpm/vue-loader@15.9.8_@vue+compiler-sfc@3.5.26_css-loader@7.1.2_@rspack+core@1.6.8_webpack@_627e006248d04719becae5a8a0ffece3/node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/test/index.vue?vue&type=template&id=6e6c385e&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"test-container"},[_c('el-divider',{attrs:{"content-position":"left"}},[_vm._v("你可以在这里写demo")])],1)}
+var staticRenderFns = []
+
+
+;// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@10.0.0_@babel+core@7.28.5_webpack@5.104.1/node_modules/babel-loader/lib/index.js??clonedRuleSet-1[0].rules[0].use!./node_modules/.pnpm/vue-loader@15.9.8_@vue+compiler-sfc@3.5.26_css-loader@7.1.2_@rspack+core@1.6.8_webpack@_627e006248d04719becae5a8a0ffece3/node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/test/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+
+/* export default */ var testvue_type_script_lang_js_ = ({
+  name: 'Test',
+  data() {
+    return {
+      show: true
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+});
+;// CONCATENATED MODULE: ./src/views/test/index.vue?vue&type=script&lang=js&
+ /* export default */ var views_testvue_type_script_lang_js_ = (testvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue-loader@15.9.8_@vue+compiler-sfc@3.5.26_css-loader@7.1.2_@rspack+core@1.6.8_webpack@_627e006248d04719becae5a8a0ffece3/node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(42574);
+;// CONCATENATED MODULE: ./src/views/test/index.vue
+
+
+
+
+
+/* normalize component */
+;
+var component = (0,componentNormalizer["default"])(
+  views_testvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* export default */ var test = (component.exports);
+
+}),
+
+}]);
